e3342915573a66ccd7c40f4cb434cd5adcb12496
AbhinandaShetty <118873296+AbhinandaShetty@users.noreply.github.com>
2022-11-24 14:35:40 +0530

    MVC

---
+++ Student/src/MVCPattern.java
@@ -0,0 +1,31 @@
+import com.student.controller.StudentController;
+import com.student.model.Student;
+import com.student.view.StudentView;
+
+public class MVCPattern {
+	  public static void main(String[] args) {
+
+	    
+	      Student model  = retriveStudentFromDatabase();
+
+	      StudentView view = new StudentView();
+
+	      StudentController controller = new StudentController(model, view);
+
+	      controller.updateView();
+
+	      
+	      controller.setStudentName("Anil");
+
+	      controller.updateView();
+	   }
+
+	   private static Student retriveStudentFromDatabase(){
+	      Student student = new Student();
+	      student.setName("Sunil");
+	      student.setRollNo("001");
+	      return student;
+	   }
+	
+
+}
